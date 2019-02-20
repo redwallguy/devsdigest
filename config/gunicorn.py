988292a0ce055071c2c462942be78b905662630a
Vibhu Someshwar <devmoney@Vibhus-MBP.fios-router.home>
2019-02-20 17:42:25 -0500

    added unix prefix to socket

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,9 +1,10 @@
 #  Listens to nginx on /tmp/nginx.socket
 
-bind = '/tmp/nginx.socket'
-
 
 def pre_fork(server, worker):
     with open('/tmp/app-initialized', 'w'):  # Creates file to tell nginx it's ready
         pass
+
+
+bind = 'unix:/tmp/nginx.socket'
 
