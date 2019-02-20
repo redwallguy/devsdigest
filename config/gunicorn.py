657d54013e37e4bcaa999241014cdf6b248a9afc
Vibhu Someshwar <devmoney@Vibhus-MBP.fios-router.home>
2019-02-20 17:33:18 -0500

    readded leading / in binding paths

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,9 +1,9 @@
 #  Listens to nginx on /tmp/nginx.socket
 
-bind = 'tmp/nginx.socket'
+bind = '/tmp/nginx.socket'
 
 
 def pre_fork(server, worker):
-    with open('tmp/app-initialized', 'w'):  # Creates file to tell nginx it's ready
+    with open('/tmp/app-initialized', 'w'):  # Creates file to tell nginx it's ready
         pass
 
