d892b6955f805be59daa0a758fd8cdde9480f47e
Vibhu Someshwar <devmoney@Vibhus-MBP.fios-router.home>
2019-03-11 14:45:44 -0400

    2048 game check logic functional, scoreboard functional

--- devsdigest/devsite/static/devsite/js/data_mains/2048main.js
+++ devsdigest/devsite/static/devsite/js/data_mains/2048main.js
@@ -5,4 +5,5 @@ requirejs.config({
     }
 });
 
-requirejs(['scripts/2048']);
+requirejs(['scripts/2048']); // var test = require("scripts/2048"); for testing on console
+// Also require and requirejs are the same object, doesn't matter which one you use
--- devsdigest/devsite/static/devsite/js/scripts/2048.js
+++ devsdigest/devsite/static/devsite/js/scripts/2048.js
@@ -1,10 +1,14 @@
-define(['jquery','./base'], function($, base) {
+define(['jquery','./base', './2048helpers'], function($, base, helper) {
+/*BEGIN INIT*/
 
    /* Creating table and inserting into DOM
     * Each td has class identifiers corresponding to position (x,y) in table.
     * (0,0) positioned at top left.
     */
-    let redo_state = [];
+    let undo_state = [];
+    let score = 0;
+    let redo_score = 0;
+    let high_score = 0;
     let table_element = $(".v2048-board");
 
     for (let i=0; i < 4 ; i++) {
@@ -19,10 +23,22 @@ define(['jquery','./base'], function($, base) {
         table_element.append(div);
     }
 
-   /* Wrapper function for swipe. Called by keydown
+    /* Bind methods to HTML elements
+     *
+     */
+     $(".v2048-reset").on("click.v2048", new_game);
+     $(".v2048-undo").on("click.v2048", undo);
+
+     /* Make new game */
+     new_game();
+
+/*END INIT*/
+   /* Wrapper function for swiping. Called by keydown
     *
     */
     function swipe(direction) {
+        undo_state = read_table();
+        redo_score = score;
         switch (direction) {
             case "left": {
                 swipe_helper("x", 1);
@@ -53,57 +69,102 @@ define(['jquery','./base'], function($, base) {
                 let gather = [];
 
                 for (let j=0; j < 4; j++) {
-                    let cell = (axis === "x" && ortho_start===0) ? get_cell(j,i) :
-                     (axis === "x" && ortho_start===1) ? get_cell(3-j,i) :
-                     (axis === "y" && ortho_start===0) ? get_cell(i,j) :
-                     (axis === "y" && ortho_start===1) ? get_cell(i,3-j) :
+                    let cell = (axis === "x" && ortho_start===0) ? helper.get_cell(j,i) :
+                     (axis === "x" && ortho_start===1) ? helper.get_cell(3-j,i) :
+                     (axis === "y" && ortho_start===0) ? helper.get_cell(i,j) :
+                     (axis === "y" && ortho_start===1) ? helper.get_cell(i,3-j) :
                      "ERROR IN GATHER";
                     if (cell.attr("data-2048-num") !== "0") {
                         gather.push(cell.attr("data-2048-num"));
                     }
                 }
 
-                console.log("Gather " + i + ": " + gather);
+                //console.log("Gather " + i + ": " + gather);
                 let buffer = 4-gather.length;
 
                 for (let j=0; j < buffer; j++) {
                     gather.unshift("0");
                 }
 
-                console.log("Shifted " + i + ": " + gather);
+                //console.log("Shifted " + i + ": " + gather);
 
                 let end_state = [];
 
                 for (let j=0; j < 4; j++) {
-                    if (j === 3) { // If we get to end of gather, it's not paired, so push and break.
+                   /* If we get to end of gather, it's not paired, so push and break. */
+                    if (j === 3) {
                         end_state.push(gather[j]);
                         break;
                     }
-                    if (j === 4) { // If we overshoot, end was paired, so break.
+                    /* If we overshoot, end was paired, so break. */
+                    if (j === 4) {
                         break;
                     }
-                    if (gather[j] === gather[j+1] && parseInt(gather[j]) !== 0) { // If consecutive entries match, push their sum and skip matched entry.
+                   /* If consecutive entries match, push their sum and skip
+                    * matched entry. Add 2^sum to score, and if greater than
+                    * high score, set high score to score.
+                    */
+                    if (gather[j] === gather[j+1] && parseInt(gather[j]) !== 0) {
                         end_state.push("0", (parseInt(gather[j])+1).toString());
+                        console.log(parseInt(gather[j])+1);
+                        score += Math.floor(Math.pow(2,parseInt(gather[j])+1));
+                        console.log(score);
+                        helper.update_score(score,high_score);
+                        if (score > high_score) {
+                          high_score = score;
+                        }
                         j++;
                     }
-                    else { // If no match, push entry.
+                    /* If no match, push entry. */
+                    else {
                         end_state.push(gather[j]);
                     }
                 }
 
-                console.log("End state: " + i + ": " + end_state);
+                //console.log("End state: " + i + ": " + end_state);
 
                 for (let j=0; j < 4; j++) {
-                    (axis === "x" && ortho_start==0) ? set_cell(j,i,end_state[j]) : // right
-                    (axis === "x" && ortho_start==1) ? set_cell(3-j,i,end_state[j]) : // left
-                    (axis === "y" && ortho_start==0) ? set_cell(i,j,end_state[j]) : // down
-                    (axis === "y" && ortho_start==1) ? set_cell(i,3-j,end_state[j]) : // up
+                    (axis === "x" && ortho_start==0) ? helper.set_cell(j,i,end_state[j]) : // right
+                    (axis === "x" && ortho_start==1) ? helper.set_cell(3-j,i,end_state[j]) : // left
+                    (axis === "y" && ortho_start==0) ? helper.set_cell(i,j,end_state[j]) : // down
+                    (axis === "y" && ortho_start==1) ? helper.set_cell(i,3-j,end_state[j]) : // up
                     console.log("End of axis/ortho chain: should never be reached.");
                 }
             }
 
 
         }
+    }
+
+    function check() {
+        let winner_cell = $("[data-2048-num='11']");
+        if (winner_cell.length !== 0) {
+            $(".v2048-game-over").css("visibility", "visible");
+            $(".v2048-game-over p").text("You are victorious!");
+            unbind();
+        }
+
+        let empty = $("[data-2048-num='0']");
+        if (empty.length !== 0) {
+            return false;
+        }
+
+        for (let i=0; i < 4; i++) {
+          for (let j=0; j < 3; j++) {
+            if (helper.get_cell(i,j).attr("data-2048-num") ===
+            helper.get_cell(i,j+1).attr("data-2048-num")) {
+              return false;
+            }
+            if (helper.get_cell(j,i).attr("data-2048-num") ===
+            helper.get_cell(j+1,i).attr("data-2048-num")) {
+              return false;
+            }
+          }
+        }
+
+        $(".v2048-game-over").css("visibility", "visible");
+        $(".v2048-game-over p").text("You lost...care to try again?");
+        unbind();
     }
 
    /* Reads board state and returns representative array
@@ -116,7 +177,7 @@ define(['jquery','./base'], function($, base) {
         let temp = [];
 
             for (let j=0; j < 4; j++) {
-                temp.push(get_cell(j,i).attr("data-2048-num"));
+                temp.push(helper.get_cell(j,i).attr("data-2048-num"));
             }
             table.push(temp);
         }
@@ -129,100 +190,85 @@ define(['jquery','./base'], function($, base) {
     function write_table(saved_table) {
         for (let i=0; i < 4; i++) {
             for (let j=0; j < 4; j++) {
-                set_cell(j,i,saved_table[i][j]);
+                helper.set_cell(j,i,saved_table[i][j]);
             }
         }
     }
 
-   /* Helper functions
-    *
-    */
-
-   /* Get jquery object of td element at position (x,y) in the table ((0,0) origin @ top left)
-    *
-    */
-    function get_cell(x,y) {
-        return $(".v2048-board div.x" + x + ".y" + y);
+    function reset() {
+        write_table([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]);
+        score = 0;
+        helper.update_score(score, high_score);
+        $(".v2048-game-over").css("visibility", "hidden");
     }
 
-   /* Set image for cell(x,y) as image from legend(n)
-    *
-    */
-    function set_cell(x,y,n) {
-        get_cell(x,y).attr("data-2048-num", n);
-        get_cell(x,y).find("img").attr("src", nth_image(n));
+    function undo() {
+        write_table(undo_state);
+        score = redo_score;
+        helper.update_score(score, high_score);
     }
 
-   /* Set image for cell given as jquery object
-    *
-    */
-    function set_cell_object(obj, n) {
-        obj.attr("data-2048-num", n);
-        obj.find("img").attr("src", nth_image(n));
-    }
-
-   /* Add either l1 or l2 image into random empty cell
-    *
-    */
-    function add_rand_cell() {
-        let empty = $("[data-2048-num='0']");
-        (rand_int(0,1) === 0) ? set_cell_object($(empty[rand_int(0,empty.length-1)]),1) :
-         set_cell_object($(empty[rand_int(0,empty.length-1)]),2);
-    }
-
-   /* Get nth image from legend table
-    *
-    */
-    function nth_image(n) {
-        return (n === 0 || n === "0") ? "" : $("table.v2048-legend td.l" + n + " img").attr("src");
-    }
-   /* Rand int in range [a,b] (i.e, double inclusive)
-    *
-    */
-    function rand_int(a,b) {
-        return Math.floor(Math.random()*(b-a) + a);
+    function new_game() {
+        reset();
+        helper.add_rand_cell();
+        helper.add_rand_cell();
+        unbind();
+        bind();
     }
 
    /* Sets up event handler for swiping using arrow keys.
     *
     */
-    $(document).keydown(function(e){
-        switch (e.which) {
-            case 38: {
-                e.preventDefault();
-                swipe("up");
-                add_rand_cell();
-                break;
-            }
-            case 40: {
-                e.preventDefault();
-                swipe("down");
-                add_rand_cell();
-                break;
-            }
-            case 37: {
-                e.preventDefault();
-                swipe("left");
-                add_rand_cell();
-                break;
-            }
-            case 39: {
-                e.preventDefault();
-                swipe("right");
-                add_rand_cell();
-                break;
-            }
-        }
-    });
+    function bind() {
+        $(document).on("keydown.v2048", function(e){
+            e.preventDefault();
 
-    set_cell(1,2,2);
-    set_cell(1,0,4);
+            switch (e.which) {
+                case 38: {
+                    swipe("up");
+                    break;
+                }
+                case 40: {
+                    swipe("down");
+                    break;
+                }
+                case 37: {
+                    swipe("left");
+                    break;
+                }
+                case 39: {
+                    swipe("right");
+                    break;
+                }
+                default: {
+                  return false;
+                }
+            }
 
-    setTimeout(function(){
-        console.log(read_table());
-        let table = [[0,0,0,0],[1,1,1,1],[2,2,2,2],[3,3,3,3]];
-        write_table(table);
-    }, 3000);
+            check();
+            helper.add_rand_cell();
+        });
+    }
+
+   /* Unbinds swiping from page (for game over screen)
+    *
+    */
+    function unbind() {
+        $(document).off("keydown.v2048");
+    }
+
+    let template_table = [[0,0,0,0],[1,1,1,1],[2,2,2,2],[3,3,3,3]];
+
+    return { // TODO streamline/remove this object and bind to elements here instead
+        swipe: swipe,
+        read_table: read_table,
+        write_table: write_table,
+        reset: reset,
+        template_table: template_table,
+        undo: undo,
+        new_game: new_game
+    }
 
     //$("table.v2048-table img").attr("src", $(".l1 img").attr("src")); // temp image in cells to check dimensions
 }); // TODO make animation for slide: fade out/in (Previous slides in dir/fades out, current fades in stationary)
+// TODO make reset key 'r' that must be held for x seconds (3?)
